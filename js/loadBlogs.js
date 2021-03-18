a2212ec24be05cf5e7ab27a78f7bf08952dc40db
KG sensei <60828719+kgsensei@users.noreply.github.com>
2021-03-18 16:09:03 -0400

    idek

--- js/loadBlogs.js
+++ js/loadBlogs.js
@@ -3,7 +3,7 @@
 fetch("/kgsensei/blogData/main.json")
      .then(response => response.json())
      .then(json => {
-          window.obj=JSON.parse(json.blogs[0]);
+          window.obj=JSON.parse(json);
 });
 
 whilLoop=window.obj["blogs"].length;
