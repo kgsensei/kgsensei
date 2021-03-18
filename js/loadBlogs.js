dd39cc6eae91956200cf7a971d53badfca1856c1
KG sensei <60828719+kgsensei@users.noreply.github.com>
2021-03-18 14:58:19 -0400

    Blog Data Flow

--- js/loadBlogs.js
+++ js/loadBlogs.js
@@ -1,1 +1,8 @@
 /* Load all blogs and associated links */
+
+jsondata=require('/kgsensei/blogData/main.json');
+obj=JSON.parse(jsondata);
+whilLoop=obj["blogs"].length;
+for(count=0;count<whilLoop;count++) {
+     document.getElementById("blogHolder").innerHTML=document.getElementById("blogHolder").innerHTML+count;
+}
