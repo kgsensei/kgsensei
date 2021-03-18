eee245d1db1628c9f7dcbe564c4454944604f968
KG sensei <60828719+kgsensei@users.noreply.github.com>
2021-03-18 17:37:08 -0400

    Update loadBlogs.js

--- js/loadBlogs.js
+++ js/loadBlogs.js
@@ -5,9 +5,10 @@ var whilLoop;
 
 $.getJSON("/kgsensei/blogData/main.json", function(json) {
      BlogJsonData=json
+     BlogJsonData=BlogJsonData.blogs
 });
 
-whilLoop=Object.keys(BlogJsonData.blogs).length;
+whilLoop=Object.keys(BlogJsonData).length;
 for(count=0;count<whilLoop;count++) {
      document.getElementById("blogs").innerHTML=document.getElementById("blogs").innerHTML+"<li>"+count+"</li>";
 }
