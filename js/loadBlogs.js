75a6bd0f7134405a91a77dd4f607d640c9e133b7
KG sensei <60828719+kgsensei@users.noreply.github.com>
2021-03-18 16:49:57 -0400

    Fix JSON Load

--- js/loadBlogs.js
+++ js/loadBlogs.js
@@ -1,16 +1,12 @@
 /* Load all blogs and associated links */
 
-var xmlhttp=new XMLHttpRequest();
-xmlhttp.onreadystatechange=function() {
-     if(this.readyState==4 && this.status==200) {
-          var blogPosts=JSON.parse(this.responseText);
-          document.getElementById("blogs").innerHTML=blogPosts.blogs;
-     }
-};
-xmlhttp.open("GET", "/kgsensei/blogData/main.json", true);
-xmlhttp.send();
+$.getJSON("/kgsensei/blogData/main.json", function(json) {
+     console.log(json);
+});
 
+/*
 whilLoop=blogPosts.length;
 for(count=0;count<whilLoop;count++) {
      document.getElementById("blogs").innerHTML=document.getElementById("blogs").innerHTML+"<li>"+count+"</li>";
 }
+*/
