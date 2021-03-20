430a758c461e7de3a54be904abd37690ef58ddce
KG sensei <60828719+kgsensei@users.noreply.github.com>
2021-03-20 14:30:42 -0400

    Fixed Blog Load?

--- js/loadBlogs.js
+++ js/loadBlogs.js
@@ -7,7 +7,9 @@ $.getJSON("/kgsensei/blogData/main.json", function(json) {
      BlogJsonData=json
 });
 
-console.log(Object.keys(BlogJsonData).forEach())
+BlogJsonData.forEach((item) => {
+     document.getElementById("blogs").innerHTML=document.getElementById("blogs").innerHTML+"<li><a href='"+item.link+"'>"+item.title+"</a></li>";
+});
 
 whilLoop=Object.keys(BlogJsonData).length;
 for(count=0;count<whilLoop;count++) {
